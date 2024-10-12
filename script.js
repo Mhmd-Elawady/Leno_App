b87c6877989ac194d0f82b7b06ce2993aa3a219a
Mohamed Elawady <mhmdelmetwally22@gmail.com>
2024-10-13 00:47:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(".navbar_mobile-menu-toggle");
+  const mobileMenu = document.querySelector(".navbar_mobile-menu-items");
+  toggleButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+  });
+});
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar--scroll");
+  } else {
+    navbar.classList.remove("navbar--scroll");
+  }
+});
